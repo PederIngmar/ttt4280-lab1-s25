1e579bff96c90a71fc26c2bad688f20a9a115599
Kristian Hjertenes <krihjer@stud.ntnu.no>
2025-02-24 14:59:11 +0100

    Utregning av standardavvik osv

--- lab2/triangulate.py
+++ lab2/triangulate.py
@@ -25,11 +25,24 @@ def delays(sample_period, data):
     D = {}
     channel_pairs = [(0, 1), (1, 2), (0, 2)]
     sampling_f = 1 / sample_period
+
+    upsample_factor = 10
     for cpair in channel_pairs:
         d1 = detrend(data[:, cpair[0]])
         d2 = detrend(data[:, cpair[1]])
-        crosscorrelation = np.abs(np.correlate(d1, d2, mode='full'))
-        lags = np.arange(-len(data)+1, len(data))
+
+        peak_idx = np.argmax(np.abs(d1))
+        start = max(0,peak_idx-1000)
+        end = min(len(d1),peak_idx+1000)
+
+        d1_window = d1[start:end]
+        d2_window = d2[start:end]
+
+        crosscorrelation = np.abs(np.correlate(d1_window, d2_window, mode='full'))
+        lags = np.arange(-len(d1_window)+1, len(d1_window))
+
+        #crosscorrelation = np.correlate(d1,d2, mode='full')
+        #lags = np.arange(-len(d1)+1, len(d1))
         max_lag = lags[np.argmax(crosscorrelation)]
         delta_t = max_lag / sampling_f
         D[cpair] = delta_t
@@ -74,10 +87,14 @@ def estimate_angle(delays):
     t12 = delays[(0, 1)]
     t13 = delays[(0, 2)]
     t23 = delays[(1, 2)]
+
     x = (t23 - t12 + 2*t13)
     y = (t23 + t12)
+
     if x < 0:
         theta = np.pi - np.arctan(np.sqrt(3)* y/x)
+    elif x == 0:
+        theta = -np.pi/2
     else:
         theta = -np.arctan(np.sqrt(3)* y/x)
 
@@ -87,8 +104,27 @@ def estimate_angle(delays):
     print(f"Estimated angle: {theta_deg:.2f} degrees")
     return theta_deg
 
+def compute_statistics():
+    angles = [0, 36, 72, 108, 144, 180]
+    estimated_angles = []
+    print("Statistics for all angles:")
+    for angle in angles:
+        bin_files = glob.glob(f"output/{angle}/*.bin")
+        for i in range(0, len(bin_files)):
+            current_bin_file = bin_files[i]
+            file_name = os.path.splitext(os.path.basename(current_bin_file))[0]
+            sample_period, data = raspi_import(f"output/{angle}", file_name)
+            d = delays(sample_period, data)
+            estimated_angles.append(estimate_angle(d))
+        print(f"Mean: {np.mean(estimated_angles):.2f}")
+        print(f"Standard deviation: {np.std(estimated_angles):.2f}")
+        print(f"Variance: {np.var(estimated_angles):.2f}")
+        estimated_angles.clear()
+
 if __name__ == "__main__":
     angles = [0, 36, 72, 108, 144, 180]
+    compute_statistics()
+    
     for angle in angles:
         bin_files = glob.glob(f"output/{angle}/*.bin")
         first_bin_file = bin_files[0]
@@ -97,3 +133,4 @@ if __name__ == "__main__":
         d = delays(sample_period, data)
         print(angle)
         estimate_angle(d)
+    
