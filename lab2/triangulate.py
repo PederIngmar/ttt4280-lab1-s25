fd4a615b6c7a0fed54049a687fbd765584fc9c46
PederIngmar <piddi056@gmail.com>
2025-02-24 10:09:27 +0100

    measuring delays

--- lab2/triangulate.py
+++ lab2/triangulate.py
@@ -1,7 +1,6 @@
 import numpy as np 
 import os
-
-angle = ""
+import glob
 
 
 def raspi_import(path, file_name, channels=5):
@@ -35,9 +34,49 @@ def delays(sample_period, data):
 
     return D
 
+
+def all_delays(angle):
+    folder_path = f"output/{angle}"
+    bin_files = glob.glob(f"{folder_path}/*.bin")
+    all_delays = []
+
+    for bin_file in bin_files:
+        file_name = os.path.splitext(os.path.basename(bin_file))[0]
+        sample_period, data = raspi_import(folder_path, file_name)
+        d = delays(sample_period, data)
+        all_delays.append(d)
+    return all_delays
+
+def delay_calculations(all_delays):
+    # Calculate average delays
+    avg_delays = {}
+    for cpair in all_delays[0].keys():
+        avg_delays[cpair] = np.mean([d[cpair] for d in all_delays])
+
+    # Calculate variance of delays
+    var_delays = {}
+    for cpair in all_delays[0].keys():
+        var_delays[cpair] = np.var([d[cpair] for d in all_delays])
+
+    # Calculate standard deviation of delays
+    std_delays = {}
+    for cpair in all_delays[0].keys():
+        std_delays[cpair] = np.std([d[cpair] for d in all_delays])
+
+    return avg_delays, std_delays
+
+
+def estimate_angle(all_delays):
+    d = 6 #cm
+    delays = all_delays
+    placements = [[], [], []]
+
+
 if __name__ == "__main__":
     path = "output/0"
-    file_name = "d-19.06.35"
-    sample_period, data = raspi_import(path, file_name)
-    delays(sample_period, data)
-  
+    file_name = "d-19.47.30"
+    
+    angles = [0, 36, 72, 108, 144, 180]
+    for angle in angles:
+        ad = all_delays(angle)
+        avg, std = delay_calculations(ad)
